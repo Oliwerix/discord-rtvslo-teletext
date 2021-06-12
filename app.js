b9d3dd6aeb833dd87333596a9bec74f948102719
oliwerix <oliwerix@gmail.com>
2021-06-12 15:17:10 +0200

    added database.sqlite

--- app.js
+++ app.js
@@ -150,7 +150,7 @@ async function messageClean(msg) {
     if (channel){
         await Channels.update({message_id: msg.id}, { where: { channel_id: channel.channel_id}})
         msg.channel.messages.fetch(channel.message_id).then( message => {
-            message.delete()
+            message.delete().catch()
         }
         ).catch(console.error)
 
